9ce63c94abdb61a199d4dfa0cd16cf952d9ce46a
Jordan Thompson <JThompson@scottlogic.com>
2021-05-04 13:28:47 +0100

    adds input options

--- src/app/mockItems.ts
+++ src/app/mockItems.ts
@@ -4,6 +4,6 @@ export const MOCKITEMS: BudgetItem[] = [
     {description: 'item1', category:'games', amount: 100, id: 0 },
     {description: 'item2', category:'groceries', amount: 30, id: 1},
     {description: 'item3', category:'bills', amount: 165.75, id: 2},
-    {description: 'item4', category:'petrol', amount: 45.60, id: 3}
-
+    {description: 'item4', category:'petrol', amount: 45.60, id: 3},
+    {description: 'item5', category: 'salary', amount: -1800.56, id: 4},
 ]
