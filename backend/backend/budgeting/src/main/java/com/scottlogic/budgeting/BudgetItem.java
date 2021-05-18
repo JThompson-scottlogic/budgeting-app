57228d33dd3e5229ba6acd199d707b61648e7151
Jordan Thompson <JThompson@scottlogic.com>
2021-05-18 16:58:41 +0100

    adds backend

---
+++ backend/backend/budgeting/src/main/java/com/scottlogic/budgeting/BudgetItem.java
@@ -0,0 +1,57 @@
+package com.scottlogic.budgeting;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class BudgetItem {
+
+    @Id
+    private int id;
+
+    private String description;
+
+    private String category;
+
+    private float amount;
+
+    private String month;
+
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public float getAmount() {
+        return amount;
+    }
+
+    public void setAmount(float amount) {
+        this.amount = amount;
+    }
+
+    public String getMonth() {
+        return month;
+    }
+
+    public void setMonth(String month) {
+        this.month = month;
+    }
+}
